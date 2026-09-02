4abbacce00d2f5ca0ca11375eb4961dc785b5061
ph1958 <33202278+ph1958@users.noreply.github.com>
2026-09-01 20:01:40 -0700

    Add DiscussionBridge social links

--- astro.config.mjs
+++ astro.config.mjs
@@ -17,6 +17,36 @@ export default defineConfig({
           label: "GitHub",
           href: "https://github.com/DiscussionBridge/docs",
         },
+        {
+          icon: "discourse",
+          label: "DiscussionBridge community forum",
+          href: "https://forum.discussionbridge.dev/",
+        },
+        {
+          icon: "blueSky",
+          label: "DiscussionBridge on Bluesky",
+          href: "https://bsky.app/profile/discussionbridge.bsky.social",
+        },
+        {
+          icon: "discord",
+          label: "DiscussionBridge on Discord",
+          href: "https://discord.gg/Y7SRQAxKq",
+        },
+        {
+          icon: "mastodon",
+          label: "DiscussionBridge on Mastodon",
+          href: "https://mastodon.social/@DiscussionBridge",
+        },
+        {
+          icon: "reddit",
+          label: "DiscussionBridge on Reddit",
+          href: "https://www.reddit.com/r/DiscussionBridge/",
+        },
+        {
+          icon: "youtube",
+          label: "DiscussionBridge on YouTube",
+          href: "https://www.youtube.com/@DiscussionBridge",
+        },
       ],
       sidebar: [
         {
